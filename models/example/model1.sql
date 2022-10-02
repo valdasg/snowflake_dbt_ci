b9cd64f501eb57ed50a3591b923a2be7f73934f0
Valdas Gylys <valdas@gmail.com>
2022-10-02 16:03:57 +0300

    test connection

--- models/example/model1.sql
+++ models/example/model1.sql
@@ -1,6 +1,6 @@
 select
     o_orderdate order_date,
-    sum(o_totalprice) total_price
+    sum(o_totalprice) total_price_of_house
 from 
 "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
 group by order_date
